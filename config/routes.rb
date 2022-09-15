2615259510ea76bb319ec6824dbf27cb29eab67d
Jocelyn Naarden <tzionone@gmail.com>
2022-09-14 23:37:24 -0400

    Finishing Rails Setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+    # route to test your configuration
+    get '/hello', to: 'application#hello_world'
+
 end
