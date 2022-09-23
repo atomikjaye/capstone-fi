bf2be95fa369947026e955c631b1b34ec1289bc8
Jocelyn Naarden <tzionone@gmail.com>
2022-09-23 16:09:37 -0400

    Added code for fetch tests

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,29 @@ import { useState, useEffect } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
+  const fetchStories = async () => {
+    const r = await fetch("/users");
+    const users = await r.json();
+    console.log("Users", users);
+    // dispatch(grabAllStories(stories));
+  }
+
   useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
+    // fetch("/users")
+    //   .then((r) => r.json())
+    //   .then((data) => console.log(data));
+
+    fetchStories();
   }, []);
+
 
   return (
     <div className="App">
       <h1>Page Count: {count}</h1>
     </div>
   );
+
+
 }
 
 export default App;
