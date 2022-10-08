aba741250f7249b80670b6903c52b934e0e43b04
AtomikJaye <tzionone@gmail.com>
2022-10-07 22:57:14 -0400

    Reset Functionality Working

--- client/src/component/Code/CodePlay.js
+++ client/src/component/Code/CodePlay.js
@@ -5,10 +5,18 @@ import "./CodePlay.css"
 
 function CodePlay({ codeBlocksData }) {
   const { codeId } = useParams();
+  // Index for currInput
   const [currCharIndex, setCurrCharIndex] = useState(0);
+  // Current Input
   const [currInput, setCurrInput] = useState("");
+
+  // Counter for Points
   const [correctCounter, setCorrectCounter] = useState(0);
   let DEBUG = true;
+
+  //**  FOR PRETTY CONSOLES
+  const CURRENT_INDEX_CONSOLE = `background-color: #E22134; padding: 0.3rem 1.5rem; font-family: Roboto; font-size: 1.2em; line-height: 1.4em; color: white;`
+  const CURRENT_CORRECT_CONSOLE = `background-color: #60c19b; padding: 0.3rem 1.5rem; font-family: Roboto; font-size: 1.2em; line-height: 1.4em; color: white;`
 
   // Put input in focus on keydown
   // Count Up Timer
@@ -20,7 +28,9 @@ function CodePlay({ codeBlocksData }) {
   // Infinitw call w/o dependency
   useEffect(() => {
     codeInputField.current.focus();
-  }, [])
+    console.log("%c RELOADING...", CURRENT_CORRECT_CONSOLE)
+    console.log(`This is the currentCharIndex: ${currCharIndex} + this is the correctCounter ${correctCounter}`)
+  }, [currCharIndex, correctCounter])
 
   // const user = useContext(UserContext);
   // if (DEBUG) console.log(codeBlocksData);
@@ -32,7 +42,7 @@ function CodePlay({ codeBlocksData }) {
 
   // function codeHTML() {
   //   strippedCode.split("").forEach((char, index) => {
-  //     let spanTag = `<span key=${char}_${index}>${char}</span>`;
+  //     let spanTag = `< span key = ${ char }_${ index } > ${ char }</span > `;
   //     FINALCODE += spanTag;
   //   })
   // }
@@ -68,6 +78,26 @@ function CodePlay({ codeBlocksData }) {
   //   if (DEBUG) console.log(currCharIndex);
   // }
 
+  const handleReset = () => {
+    // Index for currInput
+    setCurrCharIndex(0)
+    // Current Input
+    setCurrInput("")
+    // Counter for Points
+    setCorrectCounter(0)
+
+    // get ARRAY OF SPAN CLASSES
+    //REMOVE CLASSES
+    let spanCode = document.querySelectorAll(`span`);
+    console.log(spanCode)
+
+    for (let i = 0; i < spanCode.length; i++) {
+      spanCode[i].classList.remove('incorrect');
+      spanCode[i].classList.remove('correct');
+    }
+  }
+
+
   const handleKeyDown = (e) => {
     // setCurrInput(e.target.value)
     // debugger
@@ -82,11 +112,11 @@ function CodePlay({ codeBlocksData }) {
     // Assigning characters to FINALCODE ARRAY
     const characters = FINALCODE;
     // Making currInput an ARRAY
-    let typedChar = value.split("")[currCharIndex];
+    // let typedChar = value.split("")[currCharIndex];
     // debugger
-    if (DEBUG) console.log("TYPED CHAR ARRAY", currInput.split(""))
-    if (DEBUG) console.log("TYPED CHAR Index", typedChar)
-    if (DEBUG) console.log("INDEX", currCharIndex)
+    // if (DEBUG) console.log("TYPED CHAR ARRAY", currInput.split(""))
+    // if (DEBUG) console.log("TYPED CHAR Index", typedChar)
+    // if (DEBUG) console.log(`% cINDEX + ${ currCharIndex }`, CURRENT_INDEX_CONSOLE)
 
     // We testing what inputs are taken in
     // all numbers and letters
@@ -108,31 +138,40 @@ function CodePlay({ codeBlocksData }) {
         (keyCode >= 9 && keyCode <= 31) ||
         (keyCode >= 33 && keyCode <= 46) ||
         (keyCode >= 91 && keyCode <= 145))) {
-      // If character is a backspace and the Index is not "0"
+      // If character is a backspace and the Index is >= "0"
       if (keyCode === 8 && currInput.length >= 0) {
-        // iF keyCode is a backspace
-        // remove current class in current index and go back to last place
         if (DEBUG) console.log("*********************************************************")
         if (DEBUG) console.log("CURRENT INPUT BEFORE BACKSPACE", currInput)
         if (DEBUG) console.log("SUBSTRING", currInput.substring(0, currInput.length - 1))
-        // debugger
-        // currInput.split("").slice(0,-1).join("")
-        let newCurrInput = currInput.split("").slice(0, -1).join("")
+
+        // FINAL COMMENT: Here we reassign currInput to the string minus last character
+        let newCurrInput = currInput.slice(0, -1)
         setCurrInput(newCurrInput);
-        // state not updating quickly
 
         if (DEBUG) console.log("CURRENT INPUT AFTER BACKSPACE", currInput)
         if (DEBUG) console.log("CURRENT INDEX BEFORE IN BACKSPACE", currCharIndex)
         // remove one from correct
+
+        // FINAL COMMENT: Here we get the current index - 1 (state isn't updating quickly enough)
         const newCharIndex = currCharIndex - 1;
         // maybe keep track of backspaces for points
         if (DEBUG) console.log("currInput ARRAY IN Backspace", currInput.split(""))
+
+        // FINAL COMMENT: We query select from document 
         let characterSpan = document.querySelector(`span.${"index-" + newCharIndex}`);
+
+        // there is no spanClass, we skip adding or removing class from list
         if (characterSpan !== null) {
           characterSpan.classList.remove('correct');
           characterSpan.classList.remove('incorrect');
+          // Here we update Index and Correct Counter
           setCurrCharIndex(currInput.length - 1);
+          if (DEBUG) console.log(`%c Current Index after class removal ${currCharIndex}`, CURRENT_INDEX_CONSOLE)
+          setCorrectCounter(correctCounter - 1);
+          if (DEBUG) console.log(`%c Current Index after class removal ${correctCounter}`, CURRENT_CORRECT_CONSOLE)
         }
+
+
         if (DEBUG) console.log("SPAN", document.querySelector(`span.${"index-" + currCharIndex}`));
         if (DEBUG) console.log("CURRENT INDEX BEFORE IN BACKSPACE", currCharIndex)
         // debugger
@@ -221,7 +260,7 @@ function CodePlay({ codeBlocksData }) {
 
         <button className="nes-btn is-primary">Play Again</button>
         <button className="nes-btn is-error">Go Home</button>
-        <button className="nes-btn">&#8635;</button>
+        <button className="nes-btn" onClick={() => handleReset()}>&#8635;</button>
         <div></div>
 
       </div>
